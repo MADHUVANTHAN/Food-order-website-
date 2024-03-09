6237e2386c12d3248192120dab46d286c5958d6c
MADHUVANTHAN <146375325+MADHUVANTHAN@users.noreply.github.com>
2024-03-09 16:15:22 +0530

    Delete food-order directory

--- food-order/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    // include constant.php for SITEURL
-    include('../config/constants.php');
-    //destroy the session
-    session_destroy(); //unset $_SESSION['user']
-    //redirect to the login page
-    header('location:'.SITEURL.'admin/login.php');
-?>
